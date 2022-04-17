711777f88b862443412c3fc8667fcb81b132e27d
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-04-17 18:12:16 +0700

    change a lot

---
+++ phim_heo/main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title + '-' + self.description
+
+class Item(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.title
---
+++ phim_heo/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(response):
+    return HttpResponse("<h1>Hello, world. You're at the main index.</h1>")
+
+def home(response):
+    return HttpResponse("<h1>Hello, world. You're at the home page.</h1>")
