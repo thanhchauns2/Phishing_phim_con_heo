5d5aa0588aee4fcbed3e3c899497fd88f2836946
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-04-18 19:36:37 +0700

    small commit

--- phim_heo/main/models.py
+++ phim_heo/main/models.py
@@ -16,3 +16,10 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
